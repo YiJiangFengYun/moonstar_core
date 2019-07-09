648397c634e1d5463e81b2554c3c49675b5dc5a5
YiJiangFengYun <yijiangfengyun@outlook.com>
2019-07-09 09:44:39 +0800

    Add vector ONE and ZERO

--- core/common/vector.ts
+++ core/common/vector.ts
@@ -2,3 +2,6 @@ import * as glMatrix from "gl-matrix";
 
 export type Vector = glMatrix.vec2;
 export const Vector = glMatrix.vec2;
+
+export const ONE = Vector.fromValues(1, 1);
+export const ZERO = Vector.fromValues(0, 0);
