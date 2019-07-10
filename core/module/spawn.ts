ac7952414ef84143947715771264ef9c0d90e590
YiJiangFengYun <yijiangfengyun@outlook.com>
2019-07-10 13:50:38 +0800

    Fix error of the Spawn, use itself time.

--- core/module/spawn.ts
+++ core/module/spawn.ts
@@ -7,6 +7,8 @@ export class ModSpawn extends Module {
     public static NAME = "spawn";
     public interval: number; //Unit(ms), from (1 / rate) * 1000;
     public duration: number;
+    
+    private _time: number = 0;
     private _remainTime: number = 0;
     public constructor(player: emitterPlayer.EmitterPlayer) {
         super(player);
@@ -18,20 +20,22 @@ export class ModSpawn extends Module {
         this._remainTime = 0;
         this.interval = info.rate > 0 ? 1 / info.rate : Number.MAX_VALUE;
         this.duration = info.duration > 0 ? info.duration : Number.MAX_VALUE;
+        this._time = 0;
     }
 
     public update(dt: number) {
-        dt = Math.min(dt, this.duration - this.player.time);
-        if (this.interval && dt > 0) {
+        let dt2 = Math.min(dt, this.duration - this._time);
+        if (this.interval && dt2 > 0) {
             let interval = this.interval;
-            dt = this._remainTime + dt;
-            let pCount = Math.ceil(dt / interval);
-            this._remainTime = dt % interval - interval;
+            dt2 = this._remainTime + dt2;
+            let pCount = Math.ceil(dt2 / interval);
+            this._remainTime = (dt2 - interval) % interval;
             while (pCount > 0) {
                 this._createParticle();
                 --pCount;
             }
         }
+        this._time += dt;
     }
 
     private _createParticle(){
